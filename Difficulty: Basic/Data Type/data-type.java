6e30600671f2d1c584d3343b600bf5a80be4a109
work-sidd <work.siddharth@outlook.com>
2025-01-28 22:46:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Data Type/data-type.java
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.math.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            sc.nextLine();
+            String s = sc.next();
+            Solution obj = new Solution();
+            int ans = obj.dataTypeSize(s);
+            String Character = "Character";
+            if (Character.equals(s) && ans == 2) {
+                ans = 1;
+            }
+            System.out.println(ans);
+        
+System.out.println("~");
+}
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    static int dataTypeSize(String str) {
+       switch(str){
+           case "Character": return Character.BYTES;
+           case "Integer": return Integer.BYTES;
+           case "Long": return Long.BYTES;
+           case "Float": return Float.BYTES;
+           case "Double": return Double.BYTES;
+       }
+       return -1;
+    }
+}
